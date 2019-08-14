7eadfd46e92dabcccf6e713a5c6fd7299f19d57d
ever3020 <43788561+ever3020@users.noreply.github.com>
2019-08-13 23:45:39 -0600

    Add files via upload

---
+++ Tarea1Ing/Tarea1Ing/Module1.vb
@@ -0,0 +1,64 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim salario As Double
+        Dim tiempo As Integer
+        Dim name As String
+        Dim salarioNeto As Double
+        Dim respuesta As Integer
+        Console.Clear()
+
+
+        Console.WriteLine("Bienvenido! " & vbCrLf & "introduce tu nonbre!")
+        name = Console.ReadLine
+
+        Console.Clear()
+        Console.WriteLine("Hola: " & name & " ayudame digitando tu salario")
+        salario = Console.ReadLine
+        validar(salario)
+        Console.WriteLine("muy bien " & name & " ahora digita el tiempo que llevas laborando en meses!")
+        tiempo = Console.ReadLine
+        validar(tiempo)
+
+        If tiempo < 12 Then
+            Console.Clear()
+            salarioNeto = salario * 1.05
+            Console.WriteLine(name & " Tu salario es: $" & salarioNeto)
+        ElseIf tiempo >= 12 & tiempo < 24 Then
+            Console.Clear()
+            salarioNeto = salario * 1.07
+            Console.WriteLine(name & " Tu salario es: $" & salarioNeto)
+        ElseIf tiempo >= 24 & tiempo < 60 Then
+            Console.Clear()
+            salarioNeto = salario * 1.1
+            Console.WriteLine(name & " Tu salario es: $" & salarioNeto)
+        ElseIf tiempo >= 60 & tiempo < 120 Then
+            Console.Clear()
+            salarioNeto = salario * 1.15
+            Console.WriteLine(name & " Tu salario es: $" & salarioNeto)
+            Console.ReadKey()
+        ElseIf tiempo >= 120 Then
+            Console.Clear()
+            salarioNeto = salario * 1.07
+            Console.WriteLine(name & " Tu salario es: $" & salarioNeto)
+        End If
+        Console.WriteLine("Desea utilizar nuevamente el programa?")
+        Console.WriteLine("Si su respuesta es 'Si' dijite 1, si no dijite '0'")
+        respuesta = Console.ReadLine()
+        If respuesta = 1 Then
+            Main()
+        ElseIf respuesta = 0 Then
+            Console.WriteLine("Gracias por usar nuestro programa!" & name)
+        End If
+
+    End Sub
+    Sub validar(num As Integer)
+        If num < 0 Then
+            Console.WriteLine("Los numeros negativos no son validos")
+            Console.WriteLine("Seras redirijido al inicio")
+            Console.ReadKey()
+            Main()
+        End If
+    End Sub
+
+End Module
---
+++ Tarea2Ing/Tarea2Ing/Module1.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mayor As Double
+        Dim estudiantes(9, 2) As Double
+        Dim menor As Double
+        Dim promedio As Double
+        Dim buscar As Double
+        Console.WriteLine("Bienvenido maestro")
+        For i = 0 To 9
+
+
+            For a = 0 To 2
+                Console.WriteLine("Ingrese la nota " & a + 1 & "  del estudiante: " & i + 1)
+                estudiantes(i, a) = Console.ReadLine
+            Next
+            promedio = ((estudiantes(i, 0) + estudiantes(i, 1) + estudiantes(i, 2)) / 3)
+            If estudiantes(i, 0) > estudiantes(i, 1) And estudiantes(i, 0) > estudiantes(i, 2) Then
+                mayor = estudiantes(i, 0)
+            ElseIf estudiantes(i, 1) > estudiantes(i, 0) And estudiantes(i, 1) > estudiantes(i, 2) Then
+                mayor = estudiantes(i, 1)
+            ElseIf estudiantes(i, 2) > estudiantes(i, 1) And estudiantes(i, 2) > estudiantes(i, 1) Then
+                mayor = estudiantes(i, 2)
+            ElseIf estudiantes(i, 0) = estudiantes(i, 1) And estudiantes(i, 0) = estudiantes(i, 2) Then
+                menor = estudiantes(i, 0)
+                mayor = estudiantes(i, 0)
+            End If
+            If estudiantes(i, 0) < estudiantes(i, 1) And estudiantes(i, 0) < estudiantes(i, 2) Then
+                menor = estudiantes(i, 0)
+            ElseIf estudiantes(i, 1) < estudiantes(i, 0) And estudiantes(i, 1) < estudiantes(i, 2) Then
+                menor = estudiantes(i, 1)
+            ElseIf estudiantes(i, 2) < estudiantes(i, 1) And estudiantes(i, 2) < estudiantes(i, 1) Then
+                menor = estudiantes(i, 2)
+            End If
+            MsgBox(
+                   "Estudiante N°: " & i + 1 & vbCrLf &
+                   "la nota mayor es: " & mayor & vbCrLf &
+                   "la nota menor es: " & menor & vbCrLf &
+                   "El promedio es: " & promedio
+                   )
+            Console.ReadKey()
+            Console.Clear()
+        Next
+    End Sub
+
+End Module
